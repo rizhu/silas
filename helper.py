953180a4769b1dd3ee59a7d622191646eed8fa9f
Richard Hu <33536785+FiishCo@users.noreply.github.com>
2019-09-26 12:03:50 -0700

    cleanup

--- helper.py
+++ helper.py
@@ -1,5 +1,4 @@
 import math
-import sympy 
 
 def round_to_n(x, n):
 	"""
@@ -63,47 +62,3 @@ def build_from_raw_input(input_str):
 				print(f"At least one input was not a real number. Stopping build...")
 				return False
 	return elements
-
-def cmd_check(check_input, return_type = str):
-		"""
-		Allows user to execute commands at any point
-
-		check_input: Input to be checked
-		"""
-		if check_input == "exit()":
-			exit()
-		return check_input
-
-def try_input_for_type(input_str, error_string, type_func = str, cond = lambda x : True, error = ValueError):
-		"""
-		Polls for input until input is valid. Returns input.
-
-		input_str:    String printed when polling for input
-		error_string: String printed if error is caught
-		type_func:    Function converting input to desired type
-		error:		  Error to catch
-		"""
-
-		valid, return_value = False, None
-		while not valid:
-			try:
-				return_value = type_func(cmd_check(input(input_str)))
-			except error:
-				print(error_string)
-			else:
-				if cond(return_value):
-					valid = True
-				else:
-					print(error_string)
-		return return_value
-
-def div_with_fractions(numerator, denominator):
-		"""
-		Returns a fraction if inputs are not divisible
-
-		Otherwise, returns the result of division
-		"""
-		if numerator % denominator != 0:
-			return sympy.Rational('numerator/denominator')
-		else:
-			return numerator / denominator
